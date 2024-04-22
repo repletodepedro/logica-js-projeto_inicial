86ec820c3a9a5d5e0efd22dc13ab800ccde347eb
repletodepedro <contato@repletodepedro.com>
2024-04-22 10:54:39 -0300

    add number of "tentativas"

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-alert('Boas vindas ao jogo do número secreto!');
+alert ('Boas vindas ao jogo do número secreto!');
+
+let numeroSecreto = 5;
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+//enquanto o chute nao for igual ao ns
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 30');
+
+    // se o chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        // console.log(`você descobriu o número secreto ${numeroSecreto} `);
+        alert(`voce descobriu o numero secreto ${numeroSecreto} com ${tentativas} tentativas`);
+    }
+
+    else {
+        if (chute < numeroSecreto) {
+            alert(`o numero secreto é maior que ${chute}`);
+        }
+        else {
+            alert(`o numero secreto é menor que ${chute}`);
+        }
+
+        tentativas++;
+    }
+}
